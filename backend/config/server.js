d6c630ca13f9b552f518e85f1c0f696aeb90a559
Ilari Laitinen <ilari.laitinen@druid.fi>
2021-09-17 14:42:13 +0300

    Create Strapi starter project

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'd515c306770a926946ee3fe55a3476cc'),
+    },
+  },
+});
